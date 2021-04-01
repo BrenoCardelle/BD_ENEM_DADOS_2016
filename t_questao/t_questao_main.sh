d37d29f7481d196f6f6ba0225c16f3e5f1d4acd6
BrenoCardelle <43342547+BrenoCardelle@users.noreply.github.com>
2021-04-01 20:26:26 -0300

    Create t_questao_main.sh

---
+++ t_questao/t_questao_main.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+
+grep ,235, microdados_enem_2016.csv | grep 1,1,1,1 | grep -m1 -e [A-Z],1,[A-Z] -e [A-Z],0,[A-Z] > t_questao_2016.csv
+grep ,236, microdados_enem_2016.csv | grep 1,1,1,1 | grep -m1 -e [A-Z],1,[A-Z] -e [A-Z],0,[A-Z] >> t_questao_2016.csv
+grep ,237, microdados_enem_2016.csv | grep 1,1,1,1 | grep -m1 -e [A-Z],1,[A-Z] -e [A-Z],0,[A-Z] >> t_questao_2016.csv
+grep ,238, microdados_enem_2016.csv | grep 1,1,1,1 | grep -m1 -e [A-Z],1,[A-Z] -e [A-Z],0,[A-Z] >> t_questao_2016.csv
+grep ,252, microdados_enem_2016.csv | grep 1,1,1,1 | grep -m1 -e [A-Z],1,[A-Z] -e [A-Z],0,[A-Z] >> t_questao_2016.csv
+grep ,275, microdados_enem_2016.csv | grep 1,1,1,1 | grep -m1 -e [A-Z],1,[A-Z] -e [A-Z],0,[A-Z] >> t_questao_2016.csv
+grep ,276, microdados_enem_2016.csv | grep 1,1,1,1 | grep -m1 -e [A-Z],1,[A-Z] -e [A-Z],0,[A-Z] >> t_questao_2016.csv
+grep ,277, microdados_enem_2016.csv | grep 1,1,1,1 | grep -m1 -e [A-Z],1,[A-Z] -e [A-Z],0,[A-Z] >> t_questao_2016.csv
+grep ,278, microdados_enem_2016.csv | grep 1,1,1,1 | grep -m1 -e [A-Z],1,[A-Z] -e [A-Z],0,[A-Z] >> t_questao_2016.csv
+
+grep ,231, microdados_enem_2016.csv | grep 1,1,1,1 | grep -m1 -e [A-Z],1,[A-Z] -e [A-Z],0,[A-Z] >> t_questao_2016.csv
+grep ,232, microdados_enem_2016.csv | grep 1,1,1,1 | grep -m1 -e [A-Z],1,[A-Z] -e [A-Z],0,[A-Z] >> t_questao_2016.csv
+grep ,233, microdados_enem_2016.csv | grep 1,1,1,1 | grep -m1 -e [A-Z],1,[A-Z] -e [A-Z],0,[A-Z] >> t_questao_2016.csv
+grep ,234, microdados_enem_2016.csv | grep 1,1,1,1 | grep -m1 -e [A-Z],1,[A-Z] -e [A-Z],0,[A-Z] >> t_questao_2016.csv
+grep ,251, microdados_enem_2016.csv | grep 1,1,1,1 | grep -m1 -e [A-Z],1,[A-Z] -e [A-Z],0,[A-Z] >> t_questao_2016.csv
+grep ,271, microdados_enem_2016.csv | grep 1,1,1,1 | grep -m1 -e [A-Z],1,[A-Z] -e [A-Z],0,[A-Z] >> t_questao_2016.csv
+grep ,272, microdados_enem_2016.csv | grep 1,1,1,1 | grep -m1 -e [A-Z],1,[A-Z] -e [A-Z],0,[A-Z] >> t_questao_2016.csv
+grep ,273, microdados_enem_2016.csv | grep 1,1,1,1 | grep -m1 -e [A-Z],1,[A-Z] -e [A-Z],0,[A-Z] >> t_questao_2016.csv
+grep ,274, microdados_enem_2016.csv | grep 1,1,1,1 | grep -m1 -e [A-Z],1,[A-Z] -e [A-Z],0,[A-Z] >> t_questao_2016.csv
+
+grep ,239, microdados_enem_2016.csv | grep 1,1,1,1 | grep -m1 -e [A-Z],1,[A-Z] -e [A-Z],0,[A-Z] >> t_questao_2016.csv
+grep ,240, microdados_enem_2016.csv | grep 1,1,1,1 | grep -m1 -e [A-Z],1,[A-Z] -e [A-Z],0,[A-Z] >> t_questao_2016.csv
+grep ,241, microdados_enem_2016.csv | grep 1,1,1,1 | grep -m1 -e [A-Z],1,[A-Z] -e [A-Z],0,[A-Z] >> t_questao_2016.csv
+grep ,242, microdados_enem_2016.csv | grep 1,1,1,1 | grep -m1 -e [A-Z],1,[A-Z] -e [A-Z],0,[A-Z] >> t_questao_2016.csv
+grep ,253, microdados_enem_2016.csv | grep 1,1,1,1 | grep -m1 -e [A-Z],1,[A-Z] -e [A-Z],0,[A-Z] >> t_questao_2016.csv
+grep ,279, microdados_enem_2016.csv | grep 1,1,1,1 | grep -m1 -e [A-Z],1,[A-Z] -e [A-Z],0,[A-Z] >> t_questao_2016.csv
+grep ,280, microdados_enem_2016.csv | grep 1,1,1,1 | grep -m1 -e [A-Z],1,[A-Z] -e [A-Z],0,[A-Z] >> t_questao_2016.csv
+grep ,281, microdados_enem_2016.csv | grep 1,1,1,1 | grep -m1 -e [A-Z],1,[A-Z] -e [A-Z],0,[A-Z] >> t_questao_2016.csv
+grep ,282, microdados_enem_2016.csv | grep 1,1,1,1 | grep -m1 -e [A-Z],1,[A-Z] -e [A-Z],0,[A-Z] >> t_questao_2016.csv
+
+grep ,243, microdados_enem_2016.csv | grep 1,1,1,1 | grep -m1 -e [A-Z],1,[A-Z] -e [A-Z],0,[A-Z] >> t_questao_2016.csv
+grep ,244, microdados_enem_2016.csv | grep 1,1,1,1 | grep -m1 -e [A-Z],1,[A-Z] -e [A-Z],0,[A-Z] >> t_questao_2016.csv
+grep ,245, microdados_enem_2016.csv | grep 1,1,1,1 | grep -m1 -e [A-Z],1,[A-Z] -e [A-Z],0,[A-Z] >> t_questao_2016.csv
+grep ,246, microdados_enem_2016.csv | grep 1,1,1,1 | grep -m1 -e [A-Z],1,[A-Z] -e [A-Z],0,[A-Z] >> t_questao_2016.csv
+grep ,254, microdados_enem_2016.csv | grep 1,1,1,1 | grep -m1 -e [A-Z],1,[A-Z] -e [A-Z],0,[A-Z] >> t_questao_2016.csv
+grep ,283, microdados_enem_2016.csv | grep 1,1,1,1 | grep -m1 -e [A-Z],1,[A-Z] -e [A-Z],0,[A-Z] >> t_questao_2016.csv
+grep ,284, microdados_enem_2016.csv | grep 1,1,1,1 | grep -m1 -e [A-Z],1,[A-Z] -e [A-Z],0,[A-Z] >> t_questao_2016.csv
+grep ,285, microdados_enem_2016.csv | grep 1,1,1,1 | grep -m1 -e [A-Z],1,[A-Z] -e [A-Z],0,[A-Z] >> t_questao_2016.csv
+grep ,286, microdados_enem_2016.csv | grep 1,1,1,1 | grep -m1 -e [A-Z],1,[A-Z] -e [A-Z],0,[A-Z] >> t_questao_2016.csv
+
+# Extraindo dados do arquivo bruto
+python t_questao_2016.py
+
+# Eliminando linhas brancos e codigos repetidos
+sed '/^$/d' t_questao_2016_1.csv | sort -u > t_questao_2016_2.csv
+
+# Deletando arquivos
+rm t_questao_2016.csv t_questao_2016_1.csv
+
+# Extraindo dados do arquivo tratados
+python t_questao_2016_1.py
+
+# Eliminando as linhas brancos
+sed '/^$/d' t_questao_2016_3.csv  > t_questao_2016_final.csv
+
+# Colocando no final de cada linha o "null", exceto linhas que contenham [A-Z],[A-Z]
+sed -i '/[A-Z],[A-Z]/!s/$/,null/' t_questao_2016_final.csv
+
+# Deletando arquivos
+rm t_questao_2016_6.csv t_questao_2016_3.csv
+
+# Pegando o caminho
+path1=$(pwd)
+
+# Fazendo o load para a tabela
+diretorio='empty'
+nome_tabela=questao
+arquivo_original=t_questao_2016_final.csv
+source /home/bantunes/t_funcoes_shell/t_func_load.sh
+load_tabela $path1 $diretorio $nome_tabela $arquivo_original
