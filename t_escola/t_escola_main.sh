2a132b8b544c81e33079c2dffa8074ad967c8890
BrenoCardelle <43342547+BrenoCardelle@users.noreply.github.com>
2021-04-01 19:42:47 -0300

    Update t_escola_main.sh

--- t_escola/t_escola_main.sh
+++ t_escola/t_escola_main.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
 # Extraindo dados
-python t_escola_2015.py
+python t_escola_2016.py
 
 # Colocando null em campo vazio
-sed 's/, /,null/g' t_escola_2015_1.csv > t_escola_2015_2.csv
+sed 's/, /,null/g' t_escola_2016_1.csv > t_escola_2016_2.csv
 
 # Eliminando linhas duplicatas
-sort -u -k1 t_escola_2015_2.csv > t_escola_2015_final.csv
+sort -u -k1 t_escola_2016_2.csv > t_escola_2016_final.csv
 
 # Removendo arquivo
-rm t_escola_2015_1.csv t_escola_2015_2.csv
+rm t_escola_2016_1.csv t_escola_2016_2.csv
 
 # Encontrando o caminho
 path1=$(pwd)
@@ -18,6 +18,6 @@ path1=$(pwd)
 # Fazendo o load para a tabela
 diretorio='empty'
 nome_tabela=escola
-arquivo_original=t_escola_2015_final.csv
-source /home/martins/Documentos/bd_enem_dados_2015/t_funcoes_shell/t_func_load.sh
+arquivo_original=t_escola_2016_final.csv
+source /home/bantunes/t_funcoes_shell/t_func_load.sh
 load_tabela $path1 $diretorio $nome_tabela $arquivo_original
