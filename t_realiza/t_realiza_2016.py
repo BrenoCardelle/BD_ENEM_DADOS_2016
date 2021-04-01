f12ab93378a1286a108f46033894aeda53de33c5
BrenoCardelle <43342547+BrenoCardelle@users.noreply.github.com>
2021-04-01 20:27:40 -0300

    Create t_realiza_2016.py

---
+++ t_realiza/t_realiza_2016.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from script_limpeza import tirar_null
+
+import os
+
+arquivoentrada = open('microdados_enem_2016_1.csv', 'r')
+arquivosaida = open('t_realiza.csv', 'w')
+
+for linha in arquivoentrada:
+    texto = linha.split(",")
+
+    # Pegando os dados nos intervalos
+    arquivosaida.writelines(texto[0] + ",2015" + texto[92] + "," + texto[88] + "," + texto[96] + '\n')
+    arquivosaida.writelines(texto[0] + ",2015" + texto[93] + "," + texto[89] + "," + texto[97] + '\n')
+    arquivosaida.writelines(texto[0] + ",2015" + texto[94] + "," + texto[90] + "," + texto[98] + '\n')
+    arquivosaida.writelines(texto[0] + ",2015" + texto[95] + "," + texto[91] + "," + texto[99] + '\n')
+
+arquivoentrada.close()
+arquivosaida.close()
+
+arquivo = 't_realiza.csv'
+frequencia = '1'
+
+# Chamando a fucao
+tirar_null(arquivo, frequencia)
+
+# Deletando arquivo
+os.remove(arquivo)
+
+# Criar um diretorio
+if not os.path.exists("t_realiza_2016"):
+        os.makedirs("t_realiza_2016")
